a3faec119848bb4964c82b2140d8640d0e6af3e8
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-22 16:42:26 -0600

    Javascript 16 - Finding a remainder.

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,4 @@
+// This lesson showed how to find a remainder and highlighted the fact that this result can tell us if a number is even or odd.
+// Only change code below this line
+
+var remainder = 11 % 3;
