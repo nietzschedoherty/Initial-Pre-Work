34ad9468adaaa2d9703a2ea8906af94444eb39fe
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-22 15:46:46 -0600

    Javascript 6 - Understanding case sensitivity in variables.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// This lesson deomonstrated case sensitivity in Javascript and defined camelCase as the best practice.
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
