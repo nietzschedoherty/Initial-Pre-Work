273150ccc4a811e098dd633d4f5b34e662d85977
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 17:18:56 -0600

    Javascript 25 - Concatenating strings with plus sign operator.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,7 @@
+// This lesson showed how to concatenate text strings using a plus sign.
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
