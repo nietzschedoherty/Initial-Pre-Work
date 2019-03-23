0ed3ce4815e318ef108b72afa3eca5a70433236c
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 16:59:14 -0600

    Javascript 22 - Escaping literal quotes in strings.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+// This lesson showed how to display quotes inside a text string given that quotes are used to define the start and end of a text string.  The method uses a backslash before the quotes intended to appear within the text string.
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
