36e1807813da3b12514b9b1a89da6b243c8c182f
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 16:54:02 -0600

    Javascript 21 - Declare string variables.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+// This lesson showed how to declare a variable of the string type.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Stephen";
+var myLastName = "Doherty";
