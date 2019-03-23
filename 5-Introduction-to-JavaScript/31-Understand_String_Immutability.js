05c8129dedd34ebce7c840454435102d5798bd6a
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 17:45:19 -0600

    Javascript 31 - Understanding string immutability.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// This lesson was meant to show that you cannot change a certain part of a string, but you can simply reassign the string altogether.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
