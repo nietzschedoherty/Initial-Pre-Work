692e28bccb0a5676101bcb8ecf26a54353902f07
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 17:22:43 -0600

    Javascript 26 - Concatenating strings with the plus equals operator.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+// This lesson showed a shorthand for concatenating strings over multiple lines with plus equals.
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
