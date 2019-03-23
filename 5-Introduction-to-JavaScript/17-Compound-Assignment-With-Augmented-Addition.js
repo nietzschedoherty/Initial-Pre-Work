9bdf63aba358fca5b9e000fae4acacff3093cd24
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 16:43:46 -0600

    Javascript 17 - Compound assignments with augmented addition.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+// This lesson showed how to compound a variable with augmented addition as an easier and shorter way than expressing the variable equaling itself plus another number.*/
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
