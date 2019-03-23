52903f49cac42f38c1f19fcf5599c944c07d47cb
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 17:26:37 -0600

    Javascript 27 - Constructing strings with variables.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// This lesson showed how to concatenate strings while incorporating variables with the concatenation.
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Stephen Doherty";
+var myStr = "My name is " + myName + " and I am well!";
