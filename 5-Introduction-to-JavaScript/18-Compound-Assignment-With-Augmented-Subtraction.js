223dd3ee9ca1e43c7ee546a6d15ee42e371eebe2
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 16:46:56 -0600

    Javascript 18 - Compound assignment with augmented subtraction.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,10 @@
+// This lesson was akin to the previous in that it showed how to express a compound assignment, but this time using subtraction to have the variable equal itself minus a number.
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
