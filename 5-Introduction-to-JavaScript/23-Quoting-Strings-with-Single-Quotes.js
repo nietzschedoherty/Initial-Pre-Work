58ae7919a9ef5b366e901ea675c4adce16df0825
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 17:07:25 -0600

    Javascript 23 - Quoting strings with single quotes - fixed

--- 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -1,2 +1,2 @@
 // This lesson showed how to handle situations in which both single and double quotes may appear in your text string.
-var myStr = '<a href=\"http://www.example.com\" target=\"_blank\">Link</a>';
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
