04c18a4cc2e3c8f78954e9ae7e08fba567851778
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 17:13:57 -0600

    Javascript 24 - Escaping sequences in strings.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,2 @@
+// This lesson showed how to 'escape' a number of situations that require code sequences to disply certain characters in strings.
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
