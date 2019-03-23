e8b80cd5ecbbf69a5f1839ebcc048b9090f1079c
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 16:49:26 -0600

    Javascript 19 - Compound assignment with augmented multiplication.

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,10 @@
+// This lesson was like the previous two, but used a shorthand equation to express a variable equaling itself multiplied by another number.
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
