96adca46459ce6c1be0b400e8a6a480da399d868
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 14:52:15 -0600

    Javascript 44 - Manipulate arrays with the unshift function.

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,13 @@
+// This lesson showed how to add elements to the beginning of an existing array with the unshift() function.
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35])
