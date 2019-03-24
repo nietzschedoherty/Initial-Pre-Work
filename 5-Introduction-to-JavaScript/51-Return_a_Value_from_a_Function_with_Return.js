4a43938cdef5398208519819bbe9c18bf47d167e
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 15:43:36 -0600

    Javascript 51 - Return a value from a function with return command.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+// This lesson showed how to return a value from a function with the return command.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num*5;
+}
+console.log(timesFive(10));
+
+console.log(minusSeven(10));
