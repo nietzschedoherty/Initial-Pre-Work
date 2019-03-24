6382eab735fc8ce79b8a3ce88ca0e3202a48ced7
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 16:20:07 -0600

    Javascript 55 - Understanding boolean values.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+// This lesson introduces boolean datatypes as holding either a value of true or false.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
