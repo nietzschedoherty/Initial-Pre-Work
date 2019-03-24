c0f63e8f76b880be599db7d0e62ad9047e0249f2
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 15:54:48 -0600

    Javascript 53 - Assignment with a returned value.

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,20 @@
+// This lesson showed how to assign a value to a new variable using a returned value called from a function.
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
