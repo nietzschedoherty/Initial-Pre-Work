cb7213a04aec6770b80d0ccf5a179537d24c6db6
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-23 18:16:24 -0600

    Javascript 35 - Word blank testing.

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+// This lesson tested our ability to put previous lessons into action with concatenation and variables.
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+  result += "I called my "+myAdjective+" "+myNoun+", Lucy, but she "+myAdverb+" "+myVerb+" for the door.";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("cat", "little", "walked", "slowly");
