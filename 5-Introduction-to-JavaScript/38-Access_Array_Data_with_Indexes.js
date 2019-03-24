f173e6022134cd3cb17a4df044cf41afdcb79269
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 14:29:31 -0600

    Javascript 38 - Accessing array data with indexing.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// This lesson showed how to use the indexing we previously learned to reference a specific variable within an array.
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
