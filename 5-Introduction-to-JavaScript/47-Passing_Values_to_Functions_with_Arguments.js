e3256180585c84af31ce5e731d885ba5a460e177
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 15:23:42 -0600

    Javascript 47 - Passing values to functions with arguments.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,12 @@
+// This lesson showed how to define a function with two variables and then call the function by defining the variables in the call statement.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a+b);
+}
+functionWithArgs(33,57);
