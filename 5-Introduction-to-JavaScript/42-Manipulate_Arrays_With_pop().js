9421ecbbf207345bbaf1dbece53765914ba0a869
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 14:45:42 -0600

    Javascript 42 - Manipulating arrays with pop function.

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,11 @@
+// This lesson showed how to remove the last part of an array with the pop() function.
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
