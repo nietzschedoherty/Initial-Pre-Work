01e9ec503464097a760c55b6ccc5f411a02cbfb9
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 15:49:25 -0600

    Javascript 52 - Understanding undefined values returned from a function.

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,15 @@
+// This lesson showed how a function does not need to return a defined value.
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
