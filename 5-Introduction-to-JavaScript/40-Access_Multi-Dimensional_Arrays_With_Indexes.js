1fe94a6acf5e7c38f7b576556ab74a94a1266d2f
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 14:36:40 -0600

    Javascript 40 - Accessing multi-dimensional arrays with indexing.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+// This lesson showed how to access specific parts of arrays when you are dealing with nested arrays.
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
