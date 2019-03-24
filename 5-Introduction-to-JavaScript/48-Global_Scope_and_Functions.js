a3dab6babae79fd4cab35ad949a788b539bb0e2c
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 15:30:11 -0600

    Javascript 48 - Global scope and functions.

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,20 @@
+// This lesson sought to explain the difference in scope between global variables using the var keyword and other variables that may be defined without the var and inside a specific function.
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
