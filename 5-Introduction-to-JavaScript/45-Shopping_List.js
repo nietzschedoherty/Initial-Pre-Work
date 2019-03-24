21d5a431cb2f3603b73dfc8628dee0b36e8e564f
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 15:02:10 -0600

    Javascript 45 - Creating a shopping list with nested arrays.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,2 @@
+// This lesson was an exercise in creating a series of five arrays nested within one array.
+var myList = [["Bottles of milk", 3],["Yams",4],["Sausages", 6],["Dog food", 6],["Butter", 4]];
