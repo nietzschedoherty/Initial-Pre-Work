54f1f2804871632e3b29fefd70198afb5ad25d88
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-24 14:32:38 -0600

    Javascript 39 - Modify array data with indexing.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,10 @@
+// This lesson showed that array data can be modified with indexing unlike string data which requires a new declaration for the whole string.
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45
