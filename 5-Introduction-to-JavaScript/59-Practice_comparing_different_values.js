81feffa5f4d721656473254622247a72928ed524
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:04:18 -0600

    Javascript 59 - Practicing comparing different values.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,11 @@
+// This lesson was simply an illustration of the strict equality operator at work.
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
