7eaabaf28925692dc462ad64266b61416601591c
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:31:43 -0600

    Javascript 68 - Introducing Else statements.

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,17 @@
+// This lesson introduced Else statements to be performed when the conditions of an If statement are not met.
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
