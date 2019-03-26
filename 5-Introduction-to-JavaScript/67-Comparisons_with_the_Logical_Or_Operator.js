090bdd007fbf18b3f63de7a9fd2edffd3b274da0
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:28:49 -0600

    Javascript 67 - Comparing with the logical Or operator.

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+// This lesson introduced the logical Or operator, ||, for use when testing for either of two conditions to be met.
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
