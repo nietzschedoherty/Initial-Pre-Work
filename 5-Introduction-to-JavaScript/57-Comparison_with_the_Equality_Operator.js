9f3d6eb841bddbc0f921d884b70e91756df8a281
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 16:55:53 -0600

    Javascript 57 - Comparison with the equality operator.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,11 @@
+// This lesson illustrated the use of == to compare values when = is used for assigning values.
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
