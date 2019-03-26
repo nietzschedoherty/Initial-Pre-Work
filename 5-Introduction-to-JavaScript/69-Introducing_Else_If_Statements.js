9b0fbc75ffc88d093507f0aa8d94166f82791a8e
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:36:17 -0600

    Javascript 69 - Introducing Else If statements.

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,14 @@
+// This lesson showed how to use the Else If statement in cases with multiple conditions.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val <5) {
+    return "Smaller than 5";
+  } else {
+
+  return "Between 5 and 10";
+  }
+}
+
+// Change this value to test
+testElseIf(7);
