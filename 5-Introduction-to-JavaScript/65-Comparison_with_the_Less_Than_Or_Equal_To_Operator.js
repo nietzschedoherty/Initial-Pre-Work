0a39b1dff4a9c0c7fbc0537a5cf4c1f20aa01fdc
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:19:52 -0600

    Javascript 65 - Comparing with less than or equal to operator.

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,15 @@
+// This lesson introduced the comparison with the 'less than or equal to' operator within an If statement.
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
