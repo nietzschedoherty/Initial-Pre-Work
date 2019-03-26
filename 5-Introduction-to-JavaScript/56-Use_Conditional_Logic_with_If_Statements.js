9ed8b10d57aae21b69a7ba78b2a8218bde26efd8
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 16:51:19 -0600

    Javascript 56 - Using conditional If statements.

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,24 @@
+// This lesson showed how to create an 'If' statement for to perform a task based on a true or false condition.
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if (wasThatTrue){
+    return "Yes, that was true";
+
+
+  // Only change code above this line.
+}
+ return "No, that was false";
+}
+
+// Change this value to test
+trueOrFalse(true);
