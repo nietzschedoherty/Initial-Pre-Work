a057594c5f274d40552c23cb9a2b789a76636ebc
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:24:22 -0600

    Javascript 66 - Comparing with the logical And operator.

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,16 @@
+// This lesson introduced the logical And operator, &&, for use when needing to pass two comparisons for to return a true value.
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >=25) {
+
+      return "Yes";
+
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
