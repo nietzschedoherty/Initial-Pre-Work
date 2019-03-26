32976bb3b2ce5bcdb80c0122547dba26f4d51087
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 17:10:23 -0600

    Javascript 61 - Comparing with the strict inequality operator.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,16 @@
+// This lesson showed us the use of the strict inequality operator !==.
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
