56cb8444bcf68a04dbf74b25c8975f81268db1de
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 17:56:03 -0600

    Javascript 90 - Accessing nested objects.

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,15 @@
+// This lesson showed the syntax necessary to access properties in nested objects.
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
