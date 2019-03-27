defe8bc45346e1c3bbf7e45462245c2a87654006
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 21:36:01 -0600

    Javascript 79 - Counting cards exercise.

---
+++ 5-Introduction-to-JavaScript/79-Counting_Cards.js
@@ -0,0 +1,37 @@
+// This lesson was an exercise in using the switch operation in a card counting function.
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+     count ++;
+     break;
+    case 7:
+    case 8:
+    case 9:
+     count;
+     break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+     count --;
+     break;
+}
+ if(count>0) {
+    return count+" Bet";
+ }
+
+  return count+" Hold";
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
