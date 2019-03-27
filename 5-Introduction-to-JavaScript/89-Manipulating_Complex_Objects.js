f6ee22fb4e1103405183479028df6613552d2953
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 17:52:37 -0600

    Javascript 89 - Manipulating complex objects.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+// This lesson demonstrated adding an object to an array of objects.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+  ,
+  {
+    "artist": "Bob Dylan",
+    "title" : "Freewheelin'",
+    "release_year": 1962,
+    "formats": [
+      "CD",
+      "Cassette",
+      "LP"
+    ],
+    "gold": true
+  }
+];
