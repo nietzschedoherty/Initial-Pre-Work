b2a122c75526219d6c608cb314a02339c7f57fb2
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 18:54:53 -0600

    Javascript 77 - Returning boolean values from functions.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,10 @@
+// This lesson returned a boolean value from a function with a simple operator statement rather than use a longer If statement.
+function isLess(a, b) {
+  // Fix this code
+   return a < b;
+
+
+}
+
+// Change these values to test
+isLess(10, 15);
