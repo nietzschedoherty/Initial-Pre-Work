5ff8d5263e0be6872703a956c32b2469d8730feb
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 17:04:39 -0600

    Javascript 81 - Accessing object properties with dot notation.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// This lesson showed how to call certain properties from an object with the dot extention.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
