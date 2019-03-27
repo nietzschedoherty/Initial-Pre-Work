48246c1ec21c4102e949185bbbb4407176c105f7
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-26 21:44:50 -0600

    Javascript 80 - Building objects.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// This lesson introduced the building of objects using strings, numbers, and arrays.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Lucy",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Roo", "Amanita", "Litha"]
+
+};
