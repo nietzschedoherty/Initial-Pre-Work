879ac971c0c77cd95e6de219705d59a345d89454
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 17:07:07 -0600

    Javascript 82 - Accessing object properties with brackets.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,12 @@
+// This lesson showed how to call a property from an object using brackets.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
