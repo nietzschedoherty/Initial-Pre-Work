c5b148391f4fc5c567281409721a6b34bb0a5b4e
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 17:16:08 -0600

    Javascript 83 - Accessing object properties with variables.

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+// This lesson showed how to access properties of an object using variables.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
