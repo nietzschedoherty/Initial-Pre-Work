e927f1f60045580470a5acf7fc54d9b0390921f7
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 19:36:49 -0600

    Javascript 101 - Generating random fractions.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+// This lesson introduced the Math.random() function to return a random number between or including 0 up to, but not including 1.
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
