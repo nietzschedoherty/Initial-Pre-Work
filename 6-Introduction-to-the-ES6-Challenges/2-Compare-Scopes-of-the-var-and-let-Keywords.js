f3a73aa303e2462c76c4d5088106353d6ba205c2
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-28 17:27:23 -0600

    ES6 2 - Comparing scopes of var and let.

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+// This lesson is meant to illustrate the effect of the global scope of using let rather than var.
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
