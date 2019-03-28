7153bdc450303116e541d20ca9dfa5525f64d83f
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 18:29:29 -0600

    Javascript 93 - Iterating with While loops.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+// This lesson introduced a While loop and how it should work while a certain condition is met.
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i<5) {
+   myArray.push(i);
+   i++;
+}
