c647157b9e87e21da3df3393e502fbfaa3b3d1b4
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 20:01:42 -0600

    Javascript 104 - Use the parseInt function.

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+// This lesson showed how the parseInt function works to find an integer in a string.
+function convertToInteger(str) {
+  var a = parseInt(str);
+  return a;
+}
+
+convertToInteger("56");
