693de35283e5d2d9da9892a9564a6e329d85f473
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 20:13:33 -0600

    Javascript 106 - Using the ternary conditional operator.

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,6 @@
+// This lesson introduced a shorter method for an If Else statement using a conditional operator '?'.
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
