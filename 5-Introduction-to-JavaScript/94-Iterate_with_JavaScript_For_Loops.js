694ba64d3447567234fd52ec53542dfb362b0754
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-27 18:36:03 -0600

    Javascript 94 - Iterating with For loops.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+// This lesson showed how the For loop is formatted and how it executes based on its starting point for a specified number of iterations until the final condition is met.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
