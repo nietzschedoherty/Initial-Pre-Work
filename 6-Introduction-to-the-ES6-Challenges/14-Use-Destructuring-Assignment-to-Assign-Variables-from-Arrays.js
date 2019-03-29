aafe652586abdf83c6ab082387c22db54b2b852a
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-29 15:48:44 -0600

    ES6 14 - Deconstructing assingment for variables from arrays.

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,10 @@
+// This lesson demonstrated the deconstruction of variable from arrays and then assigning them.
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [b, a] = [a, b];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
