f356cc033d47a2419fe04e83ed2c900c3b4e6539
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-28 18:47:37 -0600

    ES6 10 - Using the rest operator with function parameters.

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,8 @@
+// This lesson showed how to use the rest operator in function parameters to allow for a variable range of arguments for the parameters.
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
