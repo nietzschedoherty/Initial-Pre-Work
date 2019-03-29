5585ebc8914660d7b71484fafa4528092a73af7e
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-28 18:03:24 -0600

    ES6 7 - Writing arrow functions with parameters.

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,7 @@
+// This lesson was an exercise in writing a funtion with the arrow function while allowing for parameters within the arrowed function.
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
