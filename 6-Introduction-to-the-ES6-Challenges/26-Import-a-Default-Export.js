2a542b1be0981039b31fe417dd5f1a03f1c48ae8
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-30 12:09:40 -0600

    ES6 26 - Importing a default export.

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,4 @@
+// This lesson showed us how to import a default export.  The syntax is different from importing a natural export in that no brackets are required for the imported value.
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
