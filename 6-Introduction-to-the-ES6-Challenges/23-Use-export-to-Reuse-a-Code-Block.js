7ae54a96465acac69d3d5883dcec0af64462fb16
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-30 11:54:23 -0600

    ES6 23 - Using export to reuse a code block.

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,4 @@
+// This lesson told me that I needed to use the export statement to allow functions and variables to be imported by another file.
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
