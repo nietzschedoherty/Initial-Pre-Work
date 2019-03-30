b57fa447e0a97ee2c1690270f72b327cdd6ef617
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-30 12:06:23 -0600

    ES6 25 - Creating an export fallback with export default.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,7 @@
+// The 24th lesson showed how to use * to import everything from a file.  This lesson did not appear in my project folder, but I am including it here as a preface to lesson 25.
+"use strict";
+import * as stringThing from "capitalize_strings";
+
+// This lesson showed us the use of export default as a way to export only one value.
+"use strict";
+export default function subtract(x,y) {return x - y;}
