c756d7e0a8ab4a80a9938144b81a47ac4e6c29d5
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-30 11:11:34 -0600

    ES6 19 - Writing concise declarative functions.

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+// This lesson showed the shorthand syntax for defining function thus eliminating typing the word 'function'.
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
