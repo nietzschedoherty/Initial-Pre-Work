747cfe27bd71714f75b22f76a3c07c3b0365ed54
stephennietzschedoherty <stephen.nietzsche.doherty@gmail.com>
2019-03-30 11:48:53 -0600

    ES6 22 - Difference between import and require.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,4 @@
+// This lesson introduced the use of import as a way to grab a specific function from a different file.  This is more specific than using the require function which may be overkill when only needing one fuction.
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
